0f57c8d2f75b5355bd76eff03f1cf74b7801836e
RaccoonOnion <ryan.yunxiang.yan@gmail.com>
2022-10-26 16:43:44 +0800

    driver code

--- matrix.h
+++ matrix.h
@@ -1,6 +1,34 @@
 #ifndef __MATRIX_H__
 #define __MATRIX_H__
 
+#include <stdio.h>
+#include <stdlib.h>
+
+// macro definition for bool
+typedef char bool;
+#define true 1
+#define false 0
+
+/* 
+Structure definition for matrix
+
+Name: Matrix
+
+rows: # of rows in the matrix, type: long long int
+cols: # of columns in the matrix, type: long long int
+pData: the pointer to the an array of matrix data, type: float pointer
+
+*/
+typedef struct _Matrix
+{
+    long long rows;
+    long long cols;
+    float * pData;
+} Matrix;
+
+// type defn for datatype used for rows and cols
+typedef long long type;
+
 // Matrix operation declarations are here 
 bool createMatrix(type rows, type cols, float * ptrData, Matrix * ptrMat);
 bool deleteMatrix(Matrix * ptrMat);
